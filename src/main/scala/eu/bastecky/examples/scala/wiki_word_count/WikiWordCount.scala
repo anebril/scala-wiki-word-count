a84e962c1e7fd77ffd0cddc4a4a8de67704622ca
anebril <vj8tEw5o>
2016-06-08 20:37:01 +0200

    Basic project setup - project structure, maven dependencies

---
+++ src/main/scala/eu/bastecky/examples/scala/wiki_word_count/WikiWordCount.scala
@@ -0,0 +1,8 @@
+package eu.bastecky.examples.scala.wiki_word_count
+
+/**
+  * Created by pavel on 6/8/16.
+  */
+object WikiWordCount extends App {
+    println("Hello world")
+}
---
+++ src/test/scala/eu/bastecky/examples/scala/wiki_word_count/WikiWordCount$Test.scala
@@ -0,0 +1,10 @@
+package eu.bastecky.examples.scala.wiki_word_count
+
+import org.scalatest.FunSuite
+
+/**
+  * Created by pavel on 6/8/16.
+  */
+class WikiWordCount$Test extends FunSuite {
+
+}
